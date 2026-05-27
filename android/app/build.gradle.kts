abf2ff3941795ab49b945089d4ce2c5bd6bb4d99
Federick Vidot <fakewzrd-cell@users.noreply.github.com>
2026-05-26 21:45:43 -0400

    Upgrade Android Gradle tooling so CI can produce release APKs.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.mgtlifespark.mgt_life_spark"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
